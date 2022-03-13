8785bd1d486f00525aad788401e8b6bf09203d59
Wu Xin <wuhsin@zju.edu.cn>
2022-03-13 19:13:53 +0800

    update rss engine

--- rss_engine.py
+++ rss_engine.py
@@ -1,4 +1,3 @@
-import os
 import time
 import datetime
 import pickle
@@ -6,7 +5,6 @@ import requests
 import PyRSS2Gen
 
 import url2article
-import get_url_list
 
 
 def get_url_content(url):
@@ -30,17 +28,17 @@ def is_new_article(url, url_set):
         return True
 
 
-def is_article_up_to_date(article_pubdate, days=365):
+def is_article_up_to_date(article_pubdate, within_days=365):
     '''
     判断文章发布日期是否太久远
     '''
-    if datetime.datetime.now() - article_pubdate <= datetime.timedelta(days=days):
+    if datetime.datetime.now() - article_pubdate <= datetime.timedelta(days=within_days):
         return True
     else:
         return False
 
 
-def log(msg: str, path):
+def log(msg: str, path: str):
     '''
     写入日志文件
     '''
@@ -49,93 +47,82 @@ def log(msg: str, path):
             f.writelines(msg)
 
 
-def generate_xml(verbose=False):
+def generate_xml(url_list, title_prefix_list=None, 
+                 rss_title='rss engine demo', rss_link='https://github.com/XinArkh/rss-engine', 
+                 rss_description='rss engine demo', 
+                 output='./demo.xml', database='./demo.pkl', logfile='./demo.log',
+                 within_days=365, max_item=50, verbose=False):
     '''
     生成rss xml文件
     '''
+    if not title_prefix_list: title_prefix_list = [''] * len(url_list)
+    assert len(url_list) == len(title_prefix_list)
+
     try:                        # 尝试打开本地数据库，数据库中保存url记录和rss items
-        with open(data_storage, 'rb') as f:
+        with open(database, 'rb') as f:
             url_set, item_list = pickle.load(f)
     except FileNotFoundError:   # 本地无现有数据库，则新建对应数据
         url_set = set()
         item_list = []
 
-    url_list1 = get_url_list.get_url_list_tzgg(homepage1)
-    url_list2 = get_url_list.get_url_list_yjsjy(homepage2)
-    url_list3 = get_url_list.get_url_list_xsgz(homepage3)
-
     log_str = ''
     item_num = 0
-    for url_list, title_prefix in zip([url_list1, url_list2, url_list3], ['【通知公告】', '【研究生教育】', '【学生工作】']):
-        for url in url_list:
-            if is_new_article(url, url_set):
-                try:
-                    article = url2article.get_article(url)
-                    article_link = article['url']
-                    article_title = title_prefix + article['title']
-                    article_pubdate = datetime.datetime.strptime(article['date'], '%Y-%m-%d %H:%M:%S')
-                    article_description = article['content']
+    for url, title_prefix in zip(url_list, title_prefix_list):
+        if is_new_article(url, url_set):
+            try:
+                article = url2article.get_article(url)
+                article_link = article['url']
+                article_title = title_prefix + article['title']
+                article_pubdate = datetime.datetime.strptime(article['date'], '%Y-%m-%d %H:%M:%S')
+                article_description = article['content']
 
-                    if is_article_up_to_date(article_pubdate):
-                        item_list.append(PyRSS2Gen.RSSItem(title=article_title, 
-                                                           link=article_link, 
-                                                           description=article_description, 
-                                                           guid=article_link, 
-                                                           pubDate=article_pubdate))
-                        log_str += 'item added: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
-                        if verbose: print('item added:', article_title, article['date'], article_link)
-                        item_num += 1
-                        url_set.add(url)
-                    else:
-                        log_str += 'item out of date, skipped: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
-                        if verbose: print('item out of date, skipped:', article_title, article['date'], article_link)
-                except:
-                    log_str += 'item add failed: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
-                    if verbose: print('item add failed:', article_title, article['date'], article_link)
+                if is_article_up_to_date(article_pubdate, within_days=within_days):
+                    item_list.append(PyRSS2Gen.RSSItem(title=article_title, 
+                                                       link=article_link, 
+                                                       description=article_description, 
+                                                       guid=article_link, 
+                                                       pubDate=article_pubdate))
+                    log_str += 'item added: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
+                    if verbose: print('item added:', article_title, article['date'], article_link)
+                    item_num += 1
+                    url_set.add(url)
+                else:
+                    log_str += 'item out of date, skipped: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
+                    if verbose: print('item out of date, skipped:', article_title, article['date'], article_link)
+            except:
+                log_str += 'item add failed: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
+                if verbose: print('item add failed:', article_title, article['date'], article_link)
 
-            time.sleep(0.1) # 防止访问太快被屏蔽
+        time.sleep(0.1) # 防止访问太快被屏蔽
 
     if item_num > 0:
-        # 生成xml文件
         item_list.sort(key=lambda rss_item: rss_item.pubDate, reverse=True) # 按照发表时间降序排序
-        rss = PyRSS2Gen.RSS2(title='浙江大学机械工程学院通知公告', 
-                             link='http://me.zju.edu.cn/meoffice/', 
-                             description='浙江大学机械工程学院通知公告', 
+        if len(item_list) > max_item: item_list = item_list[:max_item]      # 控制条目数量
+        
+        # 生成xml文件
+        rss = PyRSS2Gen.RSS2(title=rss_title, 
+                             link=rss_link, 
+                             description=rss_description, 
                              lastBuildDate=datetime.datetime.now(), 
                              items=item_list)
-        rss.write_xml(open(xml_path, 'w', encoding='utf-8'), encoding='utf-8')
-        if verbose: print('rss file updated.')
+        rss.write_xml(open(output, 'w', encoding='utf-8'), encoding='utf-8')
+        if verbose: print('%s: rss file updated.' %rss_title)
 
         # 写入本地数据库
-        with open(data_storage, 'wb') as f:
+        with open(database, 'wb') as f:
             pickle.dump([url_set, item_list], f)
-        if verbose: print('database updated.')
+        if verbose: print('%s: database updated.' %rss_title)
 
     # 写入日志文件
     log_str += '------ %s: %d new rss items updated ------\n\n' % (datetime.datetime.now(), item_num)
-    log(log_str, log_path)
-    if verbose: print('log file updated.')
+    log(log_str, logfile)
+    if verbose: print('%s: log file updated.' %rss_title)
 
 
 
 if __name__ == '__main__':
-    import argparse
-    
-    parser = argparse.ArgumentParser(description='Generate zju me rss feed.')
-    parser.add_argument('-o', '--output', help='the output path of the xml file',
-                        type=str, default=os.path.dirname(os.path.abspath(__file__)))
-    parser.add_argument('-d', '--database', help='The data storage path.', 
-                        type=str, default=os.path.dirname(os.path.abspath(__file__)))
-    parser.add_argument('-l', '--log', help='The log file path.', 
-                        type=str, default=os.path.dirname(os.path.abspath(__file__)))
-    args = parser.parse_args()
+    # example
+    from user_scripts.user_script_grs import fetch_src_list
+    url_list, title_prefix_list = fetch_src_list('http://www.grs.zju.edu.cn/')
 
-    xml_path = os.path.join(args.output, 'zju-me.xml')
-    data_storage = os.path.join(args.database, 'data_storage.pkl')
-    log_path = os.path.join(args.log, 'logfile.txt')
-    
-    homepage1 = 'http://me.zju.edu.cn/meoffice/'                # 通知公告
-    homepage2 = 'http://me.zju.edu.cn/meoffice/6440/list.htm'   # 研究生教育
-    homepage3 = 'http://me.zju.edu.cn/meoffice/6469/list.htm'   # 学生工作
-
-    generate_xml(verbose=True)
+    generate_xml(url_list, title_prefix_list, verbose=True)
