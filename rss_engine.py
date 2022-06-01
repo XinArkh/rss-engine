6356beb43c54da813828fada1aae5c0a1eaeb71f
Wu Xin <wuhsin@zju.edu.cn>
2022-06-02 01:43:17 +0800

    create engine class; simplify url2article

--- rss_engine.py
+++ rss_engine.py
@@ -6,127 +6,135 @@ import PyRSS2Gen
 import url2article
 
 
-def is_new_article(url, url_set):
-    '''
-    判断url是否已存在于url_set中，从而判断是否为新文章
-    '''
-    if url in url_set:
-        return False
-    else:
-        return True
+class RSSEngine:
+    """RSSEngine, generate RSS feed for arbitrary source!"""
 
-
-def is_article_up_to_date(article_pubdate, within_days=365):
-    '''
-    判断文章发布日期是否太久远
-    '''
-    if datetime.datetime.now() - article_pubdate <= datetime.timedelta(days=within_days):
-        return True
-    else:
-        return False
-
-
-def log(msg: str, path: str):
-    '''
-    写入日志文件
-    '''
-    if msg != '':
-        with open(path, 'a') as f:
-            f.writelines(msg)
-
-
-def generate_xml(url_list, title_prefix_list=None, parser=None, headers=None, 
-                 rss_title='rss engine demo', 
-                 rss_description='rss engine demo', 
+    def __init__(self, rss_title='demo', rss_description='demo', 
                  rss_link='https://github.com/XinArkh/rss-engine', 
-                 within_days=365, max_item=50, 
+                 within_days=365, max_item_num=50, 
                  output='./demo.xml', database='./demo.pkl', logfile='./demo.log', 
-                 verbose=False
-                ):
-    '''生成rss xml文件
+                 verbose=False):
+        self.rss_title = rss_title
+        self.rss_description = rss_description
+        self.rss_link = rss_link
+        self.within_days = within_days
+        self.max_item_num = max_item_num
+        self.output = output
+        self.database = database
+        self.logfile = logfile
+        self.verbose = verbose
 
-    url_list: 要生成的文章链接列表，每一个链接对应一个文章
-    title_prefix_list: 文章标题前缀，和输入的链接一一对应，可选
-    parser: 文章解析器，从一个文章链接中解析得到其信息，包括链接、日期、内容、标题，默认使用内置的URL2io解析器
-    headers: 解析器请求网页时可能用到的请求头
-    rss_title: RSS推送标题
-    rss_description: RSS推送描述
-    rss_link: RSS推送链接
-    output: xml文件输出路径
-    database: 缓存数据库路径
-    logfile: 日志文件路径
-    within_days: 限定日期，过于久远的文章不再收录
-    max_item: 最大保留文章数目
-    verbose: 是否输出详细日志，默认否
-    '''
+        self.article_parser = url2article.parse_article
+        self.article_parser_params = None
 
-    if not title_prefix_list: title_prefix_list = [''] * len(url_list)
-    assert len(url_list) == len(title_prefix_list)
-    if not parser: parser = url2article.get_article
+    def is_already_added(self, url, url_set):
+        """Whether the URL already added in the collection"""
 
-    try:                        # 尝试打开本地数据库，数据库中保存url记录和rss items
-        with open(database, 'rb') as f:
-            url_set, item_list = pickle.load(f)
-    except FileNotFoundError:   # 本地无现有数据库，则新建对应数据
-        url_set = set()
-        item_list = []
+        if url in url_set:
+            return True
+        else:
+            return False
 
-    log_str = ''
-    item_num = 0
-    for url, title_prefix in zip(url_list, title_prefix_list):
-        if is_new_article(url, url_set):
-            try:
-                article = parser(url, headers)
-                article_link = article['url']
-                article_title = title_prefix + article['title']
-                article_pubdate = datetime.datetime.strptime(article['date'], '%Y-%m-%d %H:%M:%S')
-                article_description = article['content']
-                if is_article_up_to_date(article_pubdate, within_days=within_days):
-                    item_list.append(PyRSS2Gen.RSSItem(title=article_title, 
-                                                       link=article_link, 
-                                                       description=article_description, 
-                                                       guid=article_link, 
-                                                       pubDate=article_pubdate))
-                    log_str += 'item added: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
-                    if verbose: print('item added:', article_title, article['date'], article_link)
-                    item_num += 1
-                    url_set.add(url)
-                else:
-                    log_str += 'item out of date, skipped: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
-                    if verbose: print('item out of date, skipped:', article_title, article['date'], article_link)
-            except:
-                log_str += 'item add failed: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
-                if verbose: print('item add failed:', article_title, article['date'], article_link)
+    def is_article_up_to_date(self, article_pubdate):
+        """Whether the article is up to date"""
 
-        time.sleep(0.1) # 防止访问太快被屏蔽
-
-    if item_num > 0:
-        item_list.sort(key=lambda rss_item: rss_item.pubDate, reverse=True) # 按照发表时间降序排序
-        if len(item_list) > max_item: item_list = item_list[:max_item]      # 控制条目数量
+        if datetime.datetime.now() - article_pubdate <= datetime.timedelta(days=self.within_days):
+            return True
+        else:
+            return False
         
-        # 生成xml文件
-        rss = PyRSS2Gen.RSS2(title=rss_title, 
-                             link=rss_link, 
-                             description=rss_description, 
-                             lastBuildDate=datetime.datetime.now(), 
-                             items=item_list)
-        rss.write_xml(open(output, 'w', encoding='utf-8'), encoding='utf-8')
-        if verbose: print('%s: rss file updated.' %rss_title)
+    def log(self, msg: str, path: str):
+        """record log file"""
 
-        # 写入本地数据库
-        with open(database, 'wb') as f:
+        if msg != '':
+            with open(path, 'a') as f:
+                f.writelines(msg)
+
+    def write_database(self, url_set, item_list):
+        """Write URL set and item list to local database"""
+
+        with open(self.database, 'wb') as f:
             pickle.dump([url_set, item_list], f)
-        if verbose: print('%s: database updated.' %rss_title)
+        if self.verbose: print('%s: database updated.' %self.rss_title)
 
-    # 写入日志文件
-    log_str += '------ %s: %d new rss items updated ------\n\n' % (datetime.datetime.now(), item_num)
-    log(log_str, logfile)
-    if verbose: print('%s: log file updated.' %rss_title)
+    def set_article_parser(self, parser_func, **kwargs):
+        """Set custom article parser"""
+
+        self.article_parser = parser_func
+        self.article_parser_params = kwargs
+
+    def generate_xml(self, url_list, title_prefix_list=None):
+        """Generate RSS xml file"""
+
+        if not title_prefix_list: title_prefix_list = [''] * len(url_list)
+        assert len(url_list) == len(title_prefix_list)
+
+        try:                        # try opening local database, in which url and rss items are saved
+            with open(self.database, 'rb') as f:
+                url_set, item_list = pickle.load(f)
+        except FileNotFoundError:   # if no local database is found, create a new one
+            url_set = set()
+            item_list = []
+
+        log_str = ''
+        item_num = 0
+        for url, title_prefix in zip(url_list, title_prefix_list):
+            if not self.is_already_added(url, url_set):
+                try:
+                    if self.article_parser_params:
+                        article = self.article_parser(url, **self.article_parser_params)
+                    else:
+                        article = self.article_parser(url)
+
+                    article_link = article['url']
+                    article_title = title_prefix + article['title']
+                    article_pubdate = datetime.datetime.strptime(article['date'], '%Y-%m-%d %H:%M:%S')
+                    article_description = article['content']
+
+                    if self.is_article_up_to_date(article_pubdate):
+                        item_list.append(PyRSS2Gen.RSSItem(title=article_title, 
+                                                           link=article_link, 
+                                                           description=article_description, 
+                                                           guid=article_link, 
+                                                           pubDate=article_pubdate))
+                        log_str += 'item added: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
+                        if self.verbose: print('item added:', article_title, article['date'], article_link)
+                        item_num += 1
+                        url_set.add(url)
+                    else:
+                        log_str += 'item out of date, skipped: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
+                        if self.verbose: print('item out of date, skipped:', article_title, article['date'], article_link)
+                except:
+                    log_str += 'item add failed: ' + article_title +' ' + str(article['date']) +' ' + article_link + '\n'
+                    if self.verbose: print('item add failed:', article_title, article['date'], article_link)
+
+            time.sleep(0.01) # avoid network blockdown
+
+        if item_num > 0:
+            item_list.sort(key=lambda rss_item: rss_item.pubDate, reverse=True) # descending sorting
+            if len(item_list) > self.max_item_num:                              # control item amount
+                item_list = item_list[:self.max_item_num]
+
+            # generate rss xml file
+            rss = PyRSS2Gen.RSS2(title=self.rss_title, 
+                                 link=self.rss_link, 
+                                 description=self.rss_description, 
+                                 lastBuildDate=datetime.datetime.now(), 
+                                 items=item_list)
+            
+            rss.write_xml(open(self.output, 'w', encoding='utf-8'), encoding='utf-8')
+            if self.verbose: print('%s: rss file updated.' %self.rss_title)
+            self.write_database(url_set, item_list)
+
+        log_str += '------ %s: %d new rss items updated ------\n\n' % (datetime.datetime.now(), item_num)
+        self.log(log_str, self.logfile)
+        if self.verbose: print('%s: log file updated.' %self.rss_title)
 
 
 if __name__ == '__main__':
     # example
-    from user_scripts.user_script_grs import fetch_src_list
-    url_list, title_prefix_list = fetch_src_list('http://www.grs.zju.edu.cn/')
-
-    generate_xml(url_list, title_prefix_list, verbose=True)
+    from user_scripts.user_script_grs import gen_url_list
+    url_list, title_prefix_list = gen_url_list('http://www.grs.zju.edu.cn/')
+    # generate_xml(url_list, title_prefix_list, verbose=True)
+    rss = RSSEngine(verbose=True)
+    rss.generate_xml(url_list, title_prefix_list)
--- url2article.py
+++ url2article.py
@@ -5,33 +5,19 @@ import requests
 from user_api import url2io_token, url2io_api
 
 
-def get_url_content(url, headers=None):
-    '''
-    获取网页源码，以字符串方式返回
-    '''
-    r = requests.get(url, headers=headers)
+def get_url_content(url, **kwags):
+    """Obtain the source code of the webpage, and return its text"""
+
+    r = requests.get(url, **kwags)
     r.raise_for_status()
     r.encoding = 'utf-8'
     return r.text
 
 
-def match_pubdate(html):
-    '''
-    尝试在html文本中匹配【时间：yyyy-mm-dd】格式的字符片段，若匹配到则将其作为发布时间返回
-    p.s. api自带的时间提取功能错误率比较高，经常把正文中的时间提取为发表时间
-    '''
-    searchObj = re.search(r'时间：[0-9]{4}-[0-9]{2}-[0-9]{2}|&#x65F6;&#x95F4;&#xFF1A;[0-9]{4}-[0-9]{2}-[0-9]{2}', html)
-    # 【&#x65F6;&#x95F4;&#xFF1A;】是【时间：】的unicode字符，不知道为什么有时会以此形式出现
+def parse_article(url, **kwags):
+    """Get article information, including link, date, content, and title"""
 
-    if searchObj:
-        pubdate = datetime.datetime.strptime(searchObj.group(0)[-10:], '%Y-%m-%d')
-        return pubdate
-    else:
-        return None
-
-
-def get_article(url, headers=None):
-    html = get_url_content(url, headers)
+    html = get_url_content(url, **kwags)
     query_string = {'token': url2io_token, 'url': url,}
     headers_url2io = { 'content-type': "text/html", }
     article = requests.post(url2io_api, params=query_string, headers=headers_url2io, data=html.encode('utf-8'))
@@ -42,15 +28,11 @@ def get_article(url, headers=None):
 
     article_info = article.json().copy()
 
-    pubdate = match_pubdate(html)
-    if pubdate:
-        article_info['date'] = str(pubdate)
-
     return article_info
 
 
 if __name__ == '__main__':
     # example
-    article = get_article('http://ygb.zju.edu.cn/2022/0304/c31564a2503309/page.htm')
-    print(article.keys())
+    article = parse_article('http://ygb.zju.edu.cn/2022/0304/c31564a2503309/page.htm')
     print(article)
+    print(article.keys())
