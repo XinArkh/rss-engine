738cac250a6aa2e4af6c448773c824204ec974c6
Wu Xin <wuhsin@zju.edu.cn>
2022-08-24 22:33:48 +0800

    add yunma token

--- user_api.py
+++ user_api.py
@@ -1,4 +1,8 @@
-# 注册 URL2io 账号可获得用户 token，填写到此处
+## URL2io
+# http://url2io.applinzi.com/
 url2io_token = 'xxx'
-# URL2io Article 接口地址，无需改动
 url2io_api = 'http://url2api.applinzi.com/article'
+
+## YunMa Token
+# https://www.jfbym.com/
+yunma_token = 'xxx'
